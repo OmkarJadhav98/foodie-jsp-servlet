57667a4c355608f4309167a11e56adafbecf2e4f
Omkar Jadhav <objadhav28@gmail.com>
2024-07-30 13:49:07 +0530

    commit

---
+++ src/main/java/com/onlinefoodorderingsystem/servlet/MenuServlet.java
@@ -0,0 +1,67 @@
+package src.main.java.com.onlinefoodorderingsystem.servlet;
+
+import com.onlinefoodorderingsystem.model.Menu;
+import com.onlinefoodorderingsystem.service.MenuService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/menu")
+public class MenuServlet extends HttpServlet {
+    private MenuService menuService;
+
+    @Override
+    public void init() throws ServletException {
+        menuService = new MenuService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        if ("getMenu".equals(action)) {
+            long menuId = Long.parseLong(req.getParameter("id"));
+            Menu menu = menuService.getMenuById(menuId);
+            if (menu != null) {
+                resp.setContentType("application/json");
+                resp.getWriter().write(menu.toJson());
+            } else {
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Menu not found");
+            }
+        } else {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid action");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        if ("createMenu".equals(action)) {
+            // Extract Menu details from request
+            long id = Long.parseLong(req.getParameter("id"));
+            String name = req.getParameter("name");
+            double price = Double.parseDouble(req.getParameter("price"));
+            long restaurantId = Long.parseLong(req.getParameter("restaurantId"));
+
+            Menu menu = new Menu(id, name, price, restaurantId);
+            menuService.createMenu(menu);
+            resp.setContentType("application/json");
+            resp.getWriter().write("{\"message\":\"Menu created successfully\"}");
+        } else {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid action");
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Implement update logic
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Implement delete logic
+    }
+}
